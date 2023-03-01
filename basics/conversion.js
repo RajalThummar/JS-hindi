45070fd1beaf2cd303c83c5437f220c1bf2ea46c
RajalThummar <126620116+RajalThummar@users.noreply.github.com>
2023-03-01 20:43:11 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,10 @@
+let score = 123
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 0
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);
