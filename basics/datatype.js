d275bfbaf1337029107ff9e3c02adb4d680928c2
RajalThummar <126620116+RajalThummar@users.noreply.github.com>
2023-03-01 19:44:07 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,13 @@
+
+// variable
+//number => 1 to power 53
+//BigInt
+//String => ""
+//Boolean => true/false
+//null => standalone value
+// undefinew =>
+// symbol => unique
+
+// object 
+
+console.log(typeof "rajal");
