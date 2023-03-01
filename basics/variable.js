4b70a6a697dca7252ff0cd24c0ade2c649bfee67
RajalThummar <126620116+RajalThummar@users.noreply.github.com>
2023-03-01 19:44:31 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const accountID = 9999
+let accountEmail = "rajal@gmail.com"
+let accountPassword = "1234"
+accountCity = "surat"
+
+accountEmail = "ronil@gmail.com"
+accountPassword = "0000"
+accountCity = "toronto"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
+/*
+*/
