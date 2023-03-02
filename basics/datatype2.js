f088be4e5f606865d77c5fdd3459f2238d156641
RajalThummar <126620116+RajalThummar@users.noreply.github.com>
2023-03-02 18:14:37 +0000

    datatype2

---
+++ basics/datatype2.js
@@ -0,0 +1,32 @@
+// primitive
+
+// 7 types : string, number, boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreVaule = 100.99
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id==anotherid);
+
+const bigNumber = 3333333333333333333333n
+
+// reference type or non primitive
+
+// array, objects, functions
+
+const heros = ["abc", "dfg", "yui"];
+let myObj = {
+    name: "rajal",
+    age: 20
+
+}
+
+const myFunction = function(){
+    console.log("Hello");
+}
+
