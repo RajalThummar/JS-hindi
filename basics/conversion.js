d10c0374a078723c543ba63f0d76bdb354050d21
RajalThummar <126620116+RajalThummar@users.noreply.github.com>
2023-03-02 18:14:25 +0000

    conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -8,3 +8,11 @@ console.log(valueInNumber);
 let isLoggedIn = 0
 let BooleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(BooleanIsLoggedIn);
+
+//        operations         //
+
+let str1 = "RAJAL"
+let str2 = "THUMMAR"
+
+let str3 = str1 + str2
+console.log(str3);
