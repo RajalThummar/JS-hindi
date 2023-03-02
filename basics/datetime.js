57650527e4665aab4706e72c69535b88216ac425
RajalThummar <126620116+RajalThummar@users.noreply.github.com>
2023-03-02 22:00:59 +0000

    datetime

---
+++ basics/datetime.js
@@ -0,0 +1,10 @@
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+
+let cretedate = new Date(2023, 0, 2)
+console.log(cretedate.toDateString());
+
+let mytime = Date.now()
+console.log(mytime);
