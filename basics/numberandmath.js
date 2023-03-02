3cba5875d171aa59eea9c88cd6b44ee91b57f4b8
RajalThummar <126620116+RajalThummar@users.noreply.github.com>
2023-03-02 21:01:23 +0000

    numberandmath

---
+++ basics/numberandmath.js
@@ -0,0 +1,26 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(3));
+//
+//const Number2 = 123.9999
+//console.log(Number2.toPrecision(4));
+//
+//const Number3 = 100000000000
+//console.log(Number3.toLocaleString());
+
+//***************************Maths**************************
+
+console.log(Math.abs(-9));
+console.log(Math.round(9.8));
+console.log(Math.ceil(9.1));
+console.log(Math.floor(9.8));
+console.log(Math.min(9,8,0,6,5,8));
+console.log(Math.max(9,8,7,6,5,4,3));
+console.log(Math.random());
+console.log((Math.random()*10)+ 1);
+
+
